d010ce667d04b0808f8c81a8e30372f971246236
Captainhzy <1544292143@qq.com>
2018-01-16 15:47:11 +0800

    just test

---
+++ students/hong-zhoyang/testhzy/src/com/captainhzy/test/jUnitDemo.java
@@ -0,0 +1,10 @@
+package com.captainhzy.test;
+
+import org.junit.jupiter.api.Test;
+
+public class jUnitDemo {
+    @Test
+    public void sayHello(){
+        System.out.println("hello hzy !!!");
+    }
+}
